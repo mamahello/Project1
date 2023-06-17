69fbb252d23a42b01115e89f71dc9abcc0b0f879
mamahello <1937874553@qq.com>
2023-06-17 21:04:38 +0800

    123

--- ch7/pooling_forward.py
+++ ch7/pooling_forward.py
@@ -10,8 +10,8 @@ class pooling:
 
     def forward(self,x):
         N,C,H,W=x.shape
-        out_h=int(1+(H-self.h+2*self.pad)/self.stride)
-        out_w=int(1+(W-self.w+2*self.pad)/self.stride)
+        out_h=int(1+(H-self.pool_h+2*self.pad)/self.stride)
+        out_w=int(1+(W-self.pool_w+2*self.pad)/self.stride)
 
         #展开
         col=im2col(x,self.pool_h,self.pool_w,self.stride,self.pad)
@@ -21,5 +21,6 @@ class pooling:
         out=np.max(col,axis=1)
 
         #转换格式
-        out=out.reshape.(N,out_h,out_w,C).transpose(0,3,1,2)
+        out=out.reshape(N,out_h,out_w,C).transpose(0,3,1,2)
+
         return out
